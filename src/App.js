a8a50f6da8f3f12729b7dddc0b0914c7bf37ea17
Cailenys <cailenys.salazar@gmail.com>
2020-04-07 17:51:32 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Button, Icon } from 'semantic-ui-react'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Menu>
+       <Menu.Item as={Link} to="/home">
+        Home
+       </Menu.Item>
+        </Menu>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p> Hello Wold!!!</p>
+        <h1> I'm Cailenys</h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Button size="small" color="green">
+        <Icon name="download" />
+           Download
+        </Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
